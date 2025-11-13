86a071865807fa93c5b53f66f8b802b0db2b7477
Martin Reinhardt <contact@martinreinhardt-online.de>
2025-11-13 12:31:30 +0100

    chore: Move end2end tests into extra folder

--- test/.bin/e2e-tests.sh
+++ test/.bin/e2e-tests.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
-inputFile=testfiles/addresses.xml
+inputFile=test/e2e/testfiles/addresses.xml
 ./multi_anonymizer.py -t xml -i \
     "$inputFile:(type=name,xpath=//firstname)" \
     "$inputFile:(type=name,xpath=//lastname)" \
     "$inputFile:(type=city,xpath=//city)" \
     "$inputFile:(type=zip,xpath=//zip)"
 
-inputFile=testfiles/addresses_ns.xml
+inputFile=test/e2e/testfiles/addresses_ns.xml
 ./multi_anonymizer.py -t xml -i \
     "$inputFile:(type=name,xpath=//firstname)" \
     "$inputFile:(type=name,xpath=//lastname)" \
     "$inputFile:(type=city,xpath=//city)" \
     "$inputFile:(type=zip,xpath=//zip)"
 
-inputFile=testfiles/addresses.json
+inputFile=test/e2e/testfiles/addresses.json
 ./multi_anonymizer.py -t json -i \
     "$inputFile:(type=name,jsonpath=$.addressbook.person[*].firstname)" \
     "$inputFile:(type=name,jsonpath=$.addressbook.person[*].lastname)" \
@@ -22,7 +22,7 @@ inputFile=testfiles/addresses.json
     "$inputFile:(type=zip,jsonpath=$.addressbook.person[*].address[*].zip)" \
     "$inputFile:(type=city,jsonpath=$.addressbook.person[*].address[*].city)"
 
-inputFile=testfiles/addresses_simple.json
+inputFile=test/e2e/testfiles/addresses_simple.json
 ./multi_anonymizer.py -t json -i \
     "$inputFile:(type=name,jsonpath=$.addressbook.person[*].firstname)" \
     "$inputFile:(type=name,jsonpath=$.addressbook.person[*].lastname)" \
@@ -30,17 +30,17 @@ inputFile=testfiles/addresses_simple.json
     "$inputFile:(type=zip,jsonpath=$.addressbook.person[*].address[*].zip)" \
     "$inputFile:(type=city,jsonpath=$.addressbook.person[*].address[*].city)"
 
-inputFile=testfiles/addresses.csv
+inputFile=test/e2e/testfiles/addresses.csv
 ./multi_anonymizer.py -t csv -i \
     "$inputFile:(type=number,column=1)" \
     "$inputFile:(type=address,column=2)"
 
-inputFile=testfiles/persons.csv
+inputFile=test/e2e/testfiles/persons.csv
 ./multi_anonymizer.py -t csv -i \
     "$inputFile:(type=first_name,column=1)" \
     "$inputFile:(type=last_name,column=1)"
 
-inputFile=testfiles/persons_regexp.csv
+inputFile=test/e2e/testfiles/persons_regexp.csv
 ./multi_anonymizer.py -t csv -i \
     "$inputFile:(type=name,column=1)" \
     "$inputFile:(type=email,column=1)"
